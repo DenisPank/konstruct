b74f5142a16539d166f8fb75fcae75aa416dc558
DenisPank <den3000d.1267@gmail.com>
2021-03-04 00:09:26 +0300

    Jquery initialized

---
+++ JScript/mainScript.js
@@ -0,0 +1,15 @@
+$(".scrollto").on("click", function () {
+  let href = $(this).attr("href");
+
+  $("html, body").animate(
+    {
+      scrollTop: $(href).offset().top,
+    },
+    {
+      duration: 600, // по умолчанию «400»
+      easing: "linear", // по умолчанию «swing»
+    }
+  );
+
+  return false;
+});
